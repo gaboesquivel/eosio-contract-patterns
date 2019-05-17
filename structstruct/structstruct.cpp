16bbf5ec7c38d4836aaf1afb9fd5c6fbe0ff783e
Gabo Esquivel <contact@gaboesquivel.com>
2019-05-17 12:46:53 -0600

    contracts: new add action

--- structstruct/structstruct.cpp
+++ structstruct/structstruct.cpp
@@ -11,16 +11,28 @@ CONTRACT structstruct : public eosio::contract {
   public:
     using contract::contract;
 
-    ACTION add (name account) {
+    ACTION create (name account) {
+      require_auth(account);
       vector<name> accounts{ account };
       struct_b b = { accounts };
       vector<struct_b> vector_b{ b };
       struct_a a = { vector_b };
 
       entries_table entries(_self, _self.value);
-      entries.emplace(_self, [&]( auto& row ) {
+
+      entries.emplace(account, [&]( auto& row ) {
         row.id = entries.available_primary_key();
         row.vector_a.push_back(a);
+      });
+    }
+
+    ACTION add (uint64_t id, name account) {
+      require_auth(account);
+
+      entries_table entries(_self, _self.value);
+      auto itr = entries.find(id);
+      entries.modify(itr , account, [&]( auto& row ) {
+        row.vector_a[0].vector_b[0].accounts.push_back(account);
       });
     }
 
@@ -55,4 +67,4 @@ CONTRACT structstruct : public eosio::contract {
     typedef eosio::multi_index<"entries"_n, entry > entries_table;
 };
 
-EOSIO_DISPATCH( structstruct,(add)(erase));
+EOSIO_DISPATCH( structstruct,(create)(add)(erase));
